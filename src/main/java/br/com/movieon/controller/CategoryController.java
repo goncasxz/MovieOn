f9cfff7cec325e9651022565b2c7945c0db5ee77
Vitor Samuel Gonçalves Teixeira <00120190@aluno.uniso.br>
2025-07-17 14:49:49 -0300

    feat: Adição da regra de negocio e endpoints

--- src/main/java/br/com/movieon/controller/CategoryController.java
+++ src/main/java/br/com/movieon/controller/CategoryController.java
@@ -3,10 +3,10 @@ package br.com.movieon.controller;
 import br.com.movieon.entity.Category;
 import br.com.movieon.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController()
 @RequestMapping("/movieon/category")
@@ -15,7 +15,23 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @GetMapping()
     public List<Category> getAllCategories() {
         return categoryService.findAll();
+    }
+
+    @PostMapping
+    public Category saveCategory(@RequestBody Category category) {
+        return categoryService.saveCategory(category);
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Category> getByCategoryId(@PathVariable Long id) {
+        return categoryService.findById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCategoryById(@PathVariable Long id) {
+        categoryService.deleteCategory(id);
     }
 }
--- src/main/java/br/com/movieon/service/CategoryService.java
+++ src/main/java/br/com/movieon/service/CategoryService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -15,5 +16,17 @@ public class CategoryService {
 
     public List<Category> findAll() {
         return categoryRepository.findAll();
+    }
+
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Optional<Category> findById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    public void deleteCategory(Long id){
+        categoryRepository.deleteById(id);
     }
 }
